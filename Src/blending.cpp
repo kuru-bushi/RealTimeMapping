639743b587bb1c74c2280bad9b503ebc7abeed67
kuru-bushi <kunikuni6129.sddj@gmail.com>
2024-06-10 07:23:57 +0900

    draft

---
+++ Src/blending.cpp
@@ -0,0 +1,21 @@
+
+struct params{
+    // sift パラメータ
+	int nfeatures;
+    int nOctaveLayers;
+    double contrastThreshold;
+    double edgeThreshold;
+    double sigma;
+    bool  enable_precise_upscale;
+    // その他特徴量検出
+
+    // 前処理
+    // 二値化
+    // 正規化
+    // ぼかし
+}
+
+int main(void)
+{
+    return 0;
+}
